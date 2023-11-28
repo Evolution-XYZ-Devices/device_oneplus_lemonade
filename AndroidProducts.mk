350c224ab36cb86528a53b46862c3dd31338fe2c
imminentgit <imminentgit@gmail.com>
2023-11-28 01:03:25 +0100

    lemonade: Initialize Evolution X 7.x

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
 # Copyright (C) 2021 The LineageOS Project
+#               2022 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonade.mk
+    $(LOCAL_DIR)/evolution_lemonade.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonade-user \
-    lineage_lemonade-userdebug \
-    lineage_lemonade-eng
+    evolution_lemonade-user \
+    evolution_lemonade-userdebug \
+    evolution_lemonade-eng
